aabddc62f676d8d7eb0bd2518918f4a4d94649db
unknown <toandinh080@gmail.com>
2020-06-19 17:49:07 +0700

    add fontend

---
+++ index.php
@@ -0,0 +1,358 @@
+<?php
+ include('include/header.php');
+?>
+
+
+	<div class="rev-slider">
+	<div class="fullwidthbanner-container">
+					<div class="fullwidthbanner">
+						<div class="bannercontainer" >
+					    <div class="banner" >
+								<ul>
+									<!-- THE FIRST SLIDE -->
+									<li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
+						            <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
+													<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="assets/dest/images/thumbs/1.jpg" data-src="assets/dest/images/thumbs/1.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('assets/dest/images/thumbs/1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+													</div>
+												</div>
+
+						        </li>
+								<li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
+						          <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
+												<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="assets/dest/images/thumbs/1.jpg" data-src="assets/dest/images/thumbs/1.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('assets/dest/images/thumbs/1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+											</div>
+											</div>
+
+								<li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
+						            <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
+													<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="assets/dest/images/thumbs/1.jpg" data-src="assets/dest/images/thumbs/1.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('assets/dest/images/thumbs/1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+												</div>
+											</div>
+
+						        </li>
+
+								<li data-transition="boxfade" data-slotamount="20" class="active-revslide current-sr-slide-visible" style="width: 100%; height: 100%; overflow: hidden; visibility: inherit; opacity: 1; z-index: 20;">
+						            <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
+													<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="assets/dest/images/thumbs/1.jpg" data-src="assets/dest/images/thumbs/1.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('assets/dest/images/thumbs/1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+												</div>
+											</div>
+
+						        </li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="tp-bannertimer"></div>
+					</div>
+				</div>
+				<!--slider-->
+	</div>
+	<div class="container">
+		<div id="content" class="space-top-none">
+			<div class="main-content">
+				<div class="space60">&nbsp;</div>
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="beta-products-list">
+							<h4>New Products</h4>
+							<div class="beta-products-details">
+								<p class="pull-left">438 styles found</p>
+								<div class="clearfix"></div>
+							</div>
+
+							<div class="row">
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/1.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span>$34.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
+
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/2.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span class="flash-del">$34.55</span>
+												<span class="flash-sale">$33.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/3.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span>$34.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/3.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span>$34.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div> <!-- .beta-products-list -->
+
+						<div class="space50">&nbsp;</div>
+
+						<div class="beta-products-list">
+							<h4>Top Products</h4>
+							<div class="beta-products-details">
+								<p class="pull-left">438 styles found</p>
+								<div class="clearfix"></div>
+							</div>
+							<div class="row">
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/1.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span>$34.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
+
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/2.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span class="flash-del">$34.55</span>
+												<span class="flash-sale">$33.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/3.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span>$34.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/3.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span>$34.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="space40">&nbsp;</div>
+							<div class="row">
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/1.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span>$34.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
+
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/2.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span class="flash-del">$34.55</span>
+												<span class="flash-sale">$33.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/3.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span>$34.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-3">
+									<div class="single-item">
+										<div class="single-item-header">
+											<a href="product.html"><img src="assets/dest/images/products/3.jpg" alt=""></a>
+										</div>
+										<div class="single-item-body">
+											<p class="single-item-title">Sample Woman Top</p>
+											<p class="single-item-price">
+												<span>$34.55</span>
+											</p>
+										</div>
+										<div class="single-item-caption">
+											<a class="add-to-cart pull-left" href="shopping_cart.html"><i class="fa fa-shopping-cart"></i></a>
+											<a class="beta-btn primary" href="product.html">Details <i class="fa fa-chevron-right"></i></a>
+											<div class="clearfix"></div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div> <!-- .beta-products-list -->
+					</div>
+				</div> <!-- end section with sidebar and main content -->
+
+
+			</div> <!-- .main-content -->
+		</div> <!-- #content -->
+	</div> <!-- .container -->
+
+	<?php 
+include("include/footer.php");
+	?>
+	<div class="copyright">
+		<div class="container">
+			<p class="pull-left">Privacy policy. (&copy;) 2014</p>
+			<p class="pull-right pay-options">
+				<a href="#"><img src="assets/dest/images/pay/master.jpg" alt="" /></a>
+				<a href="#"><img src="assets/dest/images/pay/pay.jpg" alt="" /></a>
+				<a href="#"><img src="assets/dest/images/pay/visa.jpg" alt="" /></a>
+				<a href="#"><img src="assets/dest/images/pay/paypal.jpg" alt="" /></a>
+			</p>
+			<div class="clearfix"></div>
+		</div> <!-- .container -->
+	</div> <!-- .copyright -->
+
+
+	<!-- include js files -->
+	<script src="assets/dest/js/jquery.js"></script>
+	<script src="assets/dest/vendors/jqueryui/jquery-ui-1.10.4.custom.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	<script src="assets/dest/vendors/bxslider/jquery.bxslider.min.js"></script>
+	<script src="assets/dest/vendors/colorbox/jquery.colorbox-min.js"></script>
+	<script src="assets/dest/vendors/animo/Animo.js"></script>
+	<script src="assets/dest/vendors/dug/dug.js"></script>
+	<script src="assets/dest/js/scripts.min.js"></script>
+	<script src="assets/dest/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+	<script src="assets/dest/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+	<script src="assets/dest/js/waypoints.min.js"></script>
+	<script src="assets/dest/js/wow.min.js"></script>
+	<!--customjs-->
+	<script src="assets/dest/js/custom2.js"></script>
+	<script>
+	$(document).ready(function($) {    
+		$(window).scroll(function(){
+			if($(this).scrollTop()>150){
+			$(".header-bottom").addClass('fixNav')
+			}else{
+				$(".header-bottom").removeClass('fixNav')
+			}}
+		)
+	})
+	</script>
+</body>
+</html>
